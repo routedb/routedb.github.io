ea042c2400375ab032b92324701f9b693cb07d5d
routedb <hazama.cd@gmail.com>
2016-09-01 14:25:30 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ var createBtn = function(key, lv, obj) {
 			out += '<button type="button" id="btnLv' + lv + '" class="btn btn-secondary btn-info btn-block" onclick="main(null, 10, null)"><strong>検索結果：「' + $("#keyword").val() + '」<strong></button>';
 			out += '</div>';
 			out += '<div class="col-md-4">';
-			out += '<button type="button" id="btnLv' + lv + '" class="btn btn-secondary btn-primary btn-block" onclick="main(' + btnKey + ', 5, this)"><strong>' +  obj.innerHTML + '</strong></button>';
+			out += '<button type="button" id="btnLv' + lv + '" class="btn btn-secondary btn-primary btn-block" onclick="main(' + btnKey + ',' + lv + ', this)"><strong>' +  obj.innerHTML + '</strong></button>';
 			out += '</div>';
 		} else {
 			out += '<div class="col-md-6"><button type="button" id="btnLv1" class="btn btn-secondary btn-info btn-block" onclick="main(null,1, this)"><span style="font-weight: bold;" id="listnull">HOME</span></button><input type="hidden" id="hidKey1" value="null"><input type="hidden" id="hidLv1" value="1"></div>';
