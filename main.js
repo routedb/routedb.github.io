5641e19334e99b116bf5954da61d04d3a549e009
routedb <hazama.cd@gmail.com>
2016-09-09 12:42:37 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -571,7 +571,7 @@ var formatterLink = function(externalLink) {
  * @return out 住所リンク用HTML文字列
  */
 var formatterStreetAddress = function(streetAddress) {
-	var out = streetAddress + '<a href ="https://maps.apple.com/?q=' + streetAddress + '" target="_blank" class="btn btn-warning btn-sm active">' + アプリで開く + '</a>';
+	var out = streetAddress + '<a href ="https://maps.apple.com/?q=' + streetAddress + '" target="_blank" class="btn btn-warning btn-sm active">アプリで開く</a>';
 	return out;
 }
 
