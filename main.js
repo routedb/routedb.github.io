1a3511dd6febe120dab25b8e5451e70b6098651b
routedb <hazama.cd@gmail.com>
2016-09-23 23:20:46 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -297,7 +297,7 @@ var createJoinLineList = function(stationCode) {
 		lineCdList.push(temp);
 	}
 	for (var x in lineCdList) {
-		out += '<button type="button" id="btnAdd" class="btn btn-secondary btn-success btn-block" onclick="main(' + lineCdList[x][0].line_cd + ', 3, this)"><strong>' + lineCdList[x][0].line_name + '&nbsp;<span class="badge badge-info">' + countData(lineCdList[x][0].line_cd, 3) + '</span></strong></button>';
+		out += '<button type="button" id="btnAdd" class="btn btn-secondary btn-success btn-block" onclick="main(' + lineCdList[x][0].line_cd + ', 3, this)"><strong>' + lineCdList[x][0].line_name + '&nbsp;<span class="badge badge-info">' + countData(stationCode, 3) + '</span></strong></button>';
 	}
 	return out;
 }
